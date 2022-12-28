b19b2e77b7fbdf2e1a9ba88f7d290098a7005230
sanchitadevi <thesanchitadevi@gmail.com>
2022-12-29 02:36:16 +0600

    tupple

--- dist/ArrayTupple.js
+++ dist/ArrayTupple.js
@@ -1,8 +1,13 @@
 "use strict";
 // array and tuple in typescript
-var players = ["Messi", "Neymar"];
+// let players = ["Messi", "Neymar"];
 // let players: string[] = ["Messi", "Neymar"]; // we can infar datatype
+// let players: (string | boolean | number)[];
+var players;
+players = ["Messi", "Neymar"];
+players = [false, true];
+// players = "2345";
 console.log(players);
-players.push("Ronaldo");
+// players.push("Ronaldo");
 var newPlayers = players.map(function (player, index) { return index + 1 + ". " + player; });
 console.log(newPlayers);
--- src/ArrayTupple.ts
+++ src/ArrayTupple.ts
@@ -1,12 +1,20 @@
 // array and tuple in typescript
 
 
-let players = ["Messi", "Neymar"];
+// let players = ["Messi", "Neymar"];
 // let players: string[] = ["Messi", "Neymar"]; // we can infar datatype
+// let players: (string | boolean | number)[];
+let players: string[] | boolean[] | number[];
+
+players = ["Messi", "Neymar"];
+
+players = [false, true];
+
+// players = "2345";
 
 console.log(players);
 
-players.push("Ronaldo");
+// players.push("Ronaldo");
 
 const newPlayers = players.map((player, index) => index + 1 + ". " + player);
 console.log(newPlayers);
